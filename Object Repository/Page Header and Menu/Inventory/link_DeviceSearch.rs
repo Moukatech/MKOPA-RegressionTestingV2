baa12dff7e2d59de1f5ca99a86d6a8047c297408
charles.chweyaa <charles.chweyaa@KE-HQ-001644.M-KOPA.local>
2018-02-06 11:09:39 +0300

    Inventory Menu Links

---
+++ Object Repository/Page Header and Menu/Inventory/link_DeviceSearch.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Link Device Search</description>
+   <name>link_DeviceSearch</name>
+   <tag></tag>
+   <elementGuidId>4ffa15f8-4df2-4f74-a3e9-1fcbd8a55cb9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;Menu&quot;]//a[text()='Device Search']</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Page Header and Menu/Inventory/link_WarehouseOperations.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Warehouse Operations Link</description>
+   <name>link_WarehouseOperations</name>
+   <tag></tag>
+   <elementGuidId>09af74b8-629e-4944-ad0b-01d94c085028</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;Menu&quot;]//a[text()='Warehouse Operations']</value>
+   </webElementProperties>
+</WebElementEntity>
