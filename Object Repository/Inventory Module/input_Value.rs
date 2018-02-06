71dd54534e43722ee35d919406419396beee13cb
stellah.ireri <stellah.ireri@KE-HQ-000926.M-KOPA.local>
2018-02-06 11:13:45 +0300

    Updates

---
+++ Object Repository/Inventory Module/input_Value.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_Value</name>
+   <tag></tag>
+   <elementGuidId>2922c682-8419-405d-b59c-d301b1ddacf9</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
