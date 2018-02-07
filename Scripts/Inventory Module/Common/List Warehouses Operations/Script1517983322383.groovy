586e041a434c1f83c4e3dbcfa17c350b96f89e37
Moukatech <lewismocha@gmail.com>
2018-02-07 09:26:05 +0300

    updates

--- Scripts/Inventory Module/Common/List Warehouses Operations/Script1517983322383.groovy
+++ Scripts/Inventory Module/Common/List Warehouses Operations/Script1517983322383.groovy
@@ -19,6 +19,14 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.callTestCase(findTestCase('Inventory Module/Common/List Warehouses Operations'), [('wareHouseCode') : wareHouseCode], 
+WebUI.callTestCase(findTestCase('Common/UserLogin'), [('Email') : 'Kennedy.Mwangi@m-kopa.com', ('Password') : 'Ken0726*-'], 
     FailureHandling.STOP_ON_FAILURE)
+
+WebUI.mouseOver(findTestObject('Page Header and Menu/Inventory/link_Inventory'))
+
+WebUI.delay(3)
+
+WebUI.click(findTestObject('Page Header and Menu/Inventory/link_WarehouseOperations'))
+
+WebUI.click(findTestObject('Inventory Module/WareHouses/link_Operations', [('code') : wareHouseCode]))
 
