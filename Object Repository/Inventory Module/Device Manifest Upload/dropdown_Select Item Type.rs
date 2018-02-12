d4973e907df3eec128b0c380caff1580e8d10838
Admin <Admin@KE-HQ-001975.M-KOPA.local>
2018-02-12 11:44:51 +0300

    peripheral upload(hammerkop)

--- Object Repository/Inventory Module/Device Manifest Upload/dropdown_Select Item Type.rs
+++ Object Repository/Inventory Module/Device Manifest Upload/dropdown_Select Item Type.rs
@@ -7,7 +7,7 @@
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value>.//*[@id='frm-upload-manifest']/div[4]/div[1]/div[2]/div/div</value>
+         <value>.//*[@id='frm-upload-manifest']/div[4]/div[1]/div[2]/div</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -17,6 +17,6 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>.//*[@id='frm-upload-manifest']/div[4]/div[1]/div[2]/div/div</value>
+      <value>.//*[@id='frm-upload-manifest']/div[4]/div[1]/div[2]/div</value>
    </webElementProperties>
 </WebElementEntity>
