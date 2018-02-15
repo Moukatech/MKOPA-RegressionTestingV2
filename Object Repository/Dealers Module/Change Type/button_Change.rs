4f893d87516f9e90ed839375cd21d3cd9d644deb
Kmwas <35446061+Kmwas@users.noreply.github.com>
2018-02-15 16:54:47 +0300

    Flag Dealer/Agent

---
+++ Object Repository/Dealers Module/Change Type/button_Change.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Change</name>
+   <tag></tag>
+   <elementGuidId>81411446-98ef-4fcf-b34e-308518f36203</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>.//*[@id='ajaxSubmit']</value>
+   </webElementProperties>
+</WebElementEntity>
