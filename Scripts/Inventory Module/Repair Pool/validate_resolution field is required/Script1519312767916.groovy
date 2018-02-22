72c24e343d008f05cb4e7cc003ddf942a1da4a46
Stellah.Ireri <Stellah.Ireri@KE-HQ-000926.M-KOPA.local>
2018-02-22 18:43:08 +0300

    Validations

---
+++ Scripts/Inventory Module/Repair Pool/validate_resolution field is required/Script1519312767916.groovy
@@ -0,0 +1,36 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.callTestCase(findTestCase('Inventory Module/Common/List Warehouses Operations'), [('wareHouseCode') : wareHouseCode], 
+    FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Inventory Module/WareHouse Operations/link_Repair pool'))
+
+WebUI.click(findTestObject('Inventory Module/Repair pool devices/link_Repair', [('deviceSerial') : deviceSerial]))
+
+WebUI.delay(3)
+
+WebUI.setText(findTestObject('Inventory Module/Repair Device/input_Repair Notes'), repairNotes )
+
+WebUI.click(findTestObject('Inventory Module/Repair Device/button_Save'))
+
+WebUI.verifyElementPresent(findTestObject('Inventory Module/Repair Device/errorMsg_Select resolution reason'), 2)
+
