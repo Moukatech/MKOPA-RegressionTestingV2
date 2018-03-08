c42757fa19ddcfb6f7911384c350fed9182aee89
Dennis.Gituto <Dennis.Gituto@KE-HQ-001711.M-KOPA.local>
2018-03-08 11:27:25 +0300

    Clean Up

---
+++ Object Repository/Customer Module/Customer Profile/image_More Device Info.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>image_More Device Info</name>
+   <tag></tag>
+   <elementGuidId>5cfc5a7a-92f1-48c8-b7e7-7c9bfeaf8867</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>.//*[@id='more-device-info-link']/img</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Inventory Module/WareHouse Operations/label_Quick Stock Info.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_Quick Stock Info</name>
+   <tag></tag>
+   <elementGuidId>6144325f-561e-4101-8fa6-6e9669fa567f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//h2[contains(.,'Quick Stock Info')]</value>
+   </webElementProperties>
+</WebElementEntity>
